f497e9b414570dd534fa6f123e9b3191238e21db
Denis <tonopik@gmail.com>
2023-03-16 20:02:19 +0200

    site structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Items from "./components/Items";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: "Chair",
+          img: "chair.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+          category: "chairs",
+          price: "19.99$",
+        },
+        {
+          id: 2,
+          title: "Table",
+          img: "table.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+          category: "tables",
+          price: "39.99$",
+        },
+        {
+          id: 3,
+          title: "Couch",
+          img: "couch.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+          category: "chairs",
+          price: "119.99$",
+        },
+        {
+          id: 4,
+          title: "Lamp",
+          img: "lamp.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+          category: "lamps",
+          price: "9.99$",
+        },
+        {
+          id: 5,
+          title: "Cabinet",
+          img: "cabinet.jpg",
+          desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit",
+          category: "cabinets",
+          price: "89.99$",
+        },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items} />
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function header() {
+  return (
+    <header>
+      <div>
+        <span className="logo">House Staff</span>
+        <ul className="nav">
+          <li>About us</li>
+          <li>Contacts</li>
+          <li>Home</li>
+        </ul>
+      </div>
+      <div className="bckgnd"></div>
+    </header>
+  );
+}
